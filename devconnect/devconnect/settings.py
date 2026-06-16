fdd82423b51377b6b94531b0ba6d38265b2b0924
sparshhere <sparshsharma2601@gmail.com>
2026-06-16 15:23:10 +0530

    DEBUG set to False

--- devconnect/devconnect/settings.py
+++ devconnect/devconnect/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r7!jk42vfdk&ibrq*haf)%%1#$qys=&784hg#i()lj=5fa1bf('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'devconnect-django.onrender.com',
